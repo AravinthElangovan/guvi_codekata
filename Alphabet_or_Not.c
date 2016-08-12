fa849aa134358388efc1d16f668edaa1ffb8aea4
AravinthElangovan <aravinthelangovan@gmail.com>
2016-08-12 12:37:59 +0530

    Create Alphabet_or_Not.c

---
+++ Alphabet_or_Not.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+    char n;
+clrscr();
+        scanf("%c",&n);
+    if( (n>='a'&& n<='z') || (n>='A' && c<='Z'))
+       printf("%c is an alphabet",n);
+    else
+       printf("%c is an Not an alphabet",n);
+getch();
+}
