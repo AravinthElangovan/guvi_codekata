0d6bbb12ee4d973d1cbf992ddcf4d6dccfb302ee
AravinthElangovan <aravinthelangovan@gmail.com>
2016-10-21 11:28:38 +0530

    Update towerofhanoi.c

--- Pro/towerofhanoi.c
+++ Pro/towerofhanoi.c
@@ -1,18 +1,18 @@
 #include <stdio.h>
-void towerOfHanoi(int n, char fromstick, char tostick, char midstick)
+void TH(int n, char fromstick, char tostick, char midstick)
 {
     if (n == 1)
     {
         printf("\n Move disk 1 from stick %c to stick %c", fromstick, tostick);
         return;
     }
-    towerOfHanoi(n-1, fromstick, midstick, tostick);
+    TH(n-1, fromstick, midstick, tostick);
     printf("\n Move disk %d from stick %c to stick %c", n, fromstick, tostick);
-    towerOfHanoi(n-1, midstick, tostick, fromstick);
+    TH(n-1, midstick, tostick, fromstick);
 }
 int main()
 {
     int n=3;
-         towerOfHanoi(n, 'A', 'C', 'B');  
+         TH(n, 'A', 'C', 'B');  
     return 0;
 }
